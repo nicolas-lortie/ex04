5f9ab467e8409634aab858abf00073ae2caaef0e
nicolas-lortie <nic.lortie@gmail.com>
2014-09-22 17:18:46 -0400

    ex04_1 upload

--- bin/ex04_1.dart
+++ bin/ex04_1.dart
@@ -1,5 +1,23 @@
-void main() {
-  //Print the first 10 numbers, starting with 0, of the Fibonacci sequence.
-  
-  
+//Print the first 10 numbers, starting with 0, of the Fibonacci sequence.
+
+String sequence(var a) {
+  int b = (0);
+  int c = (1);
+  int d = (1);
+  int e = (1);
+  String begin = '0, 1';
+  while (e < a) {
+    d = b + c;
+    begin = ('$begin, $d');
+    b = c;
+    c = d;
+    e = e + 1;
+  }
+  return begin;
+}
+main() {
+  int stop = 9;
+  String fibonacci = sequence(stop);
+  print('Exercice 1 \nThe first ten numbers of the fibonacci sequence, starting with 0 are:');
+  print('$fibonacci');
 }
--- bin/ex04_5.dart
+++ bin/ex04_5.dart
@@ -1,5 +1,16 @@
-void main() {
-  //Calculate the first 16 terms of the 8 times table (Google: 8 times table).
-  
-  
+
+//Calculate the first 16 terms of the 8 times table (Google: 8 times table).
+
+String sequence(var h) {
+  for (var h = 1; h <= 16; h++);
+  int mult = (8);
+  var result = (h * mult);
+  return result;
 }
+
+main() {
+  print('Exercice 5\nThe first 16 terms of the 8 times table are:');
+  print(result);
+
+}
+
