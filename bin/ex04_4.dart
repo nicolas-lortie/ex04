f37bedce58456be489bf96300dd51361b449563f
nicolas-lortie <nic.lortie@gmail.com>
2014-09-22 20:41:48 -0400

    ex04_4 commit

--- bin/ex04_4.dart
+++ bin/ex04_4.dart
@@ -1,7 +1,20 @@
+import 'dart:math';
+
 void main() {
   /* Calculate a compound interest each year for 5 years, for
     the initial investment of 1000$ with the fixed interest rate of 4.8.
     * 
      */
-  
+
+  print('Exercice 4.');
+  for (var year = 1; year <= 5; year++) {
+    num interest = 1.048;
+    int initial = 1000;
+    var calc = ((pow(interest, year) * initial).toStringAsFixed(2));
+    print('Investment with interests at the end of year $year : $calc');
+
+
+
+  }
+
 }
