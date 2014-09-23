01fa574468505bf866d13e767ffa508fbedebca2
nicolas-lortie <nic.lortie@gmail.com>
2014-09-22 22:05:12 -0400

    ex04_3 commit

--- bin/ex04_3.dart
+++ bin/ex04_3.dart
@@ -3,5 +3,16 @@ void main() {
      your birthday at midnight and today at midnight.
     * 
     */
-  
+
+  var born = DateTime.parse("1991-11-08 00:00");
+  var now = new DateTime.now(); // DateTime.now is not at midnight see line 12 for workaround
+  var time = now.difference(born).inDays;
+  var years = ((time / 365.25).truncate());
+  var avg = (365.25 / 12); // Average number of days in a month
+  var months = (((time % 365.25) / avg).truncate());
+  var days = (((time % 365.25) % avg).truncate()); // Applying the truncate method on the number of days solve the problem of DateTime.now not being at midnight
+  print('Exercice 3\nThe time passed since I was born is: $years years, $months months, $days days');
+
+
+
 }
