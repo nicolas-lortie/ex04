1b1c29a7efe0ddde829c8ccbe48375f82f71c63e
nicolas-lortie <nic.lortie@gmail.com>
2014-09-22 23:10:14 -0400

    ex04_2 commit

--- bin/ex04_2.dart
+++ bin/ex04_2.dart
@@ -1,3 +1,5 @@
+import 'dart:math' as math;
+
 void main() {
   /*There is a point in the center of a rectangle with the width of 800 
     and the height of 600 pixels (or millimeters on paper). The north-west
@@ -14,6 +16,24 @@ void main() {
     the angle of 45 degrees, calculate the end point of the line.
     * 
      */
+
+  var width = (800);
+  var height = (600);
+  var center_x = (width / 2);
+  var center_y = (height / 2);
+  var angledegree = (45);
+  var angleradian = ((angledegree * 2 * math.PI) / 360);
+  int hypotenuse = (160);
+
+  // Since the angle is of 45 degrees, the displacement on the X and Y axis will be the same.
+
+  var displacement = hypotenuse * math.sin(angleradian);
+  var finish_x = ((center_x + displacement).toStringAsFixed(2));
+  var finish_y = ((center_y + displacement).toStringAsFixed(2));
+
+  print('Exercice 2\nThe coordinates (x,y) of the endpoint are: ($finish_x,$finish_y)');
+
+
 
 
 }
